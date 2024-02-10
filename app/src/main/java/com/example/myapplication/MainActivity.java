faa3bf64d0f93117beafa838220c0fc2fde8d1e6
Zhang Rick <zhang.rick@northeastern.edu>
2024-02-10 09:50:29 +0000

    A1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.main_btn_aboutme).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "Name: Zhang.rick , Email: zhang.rick@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
